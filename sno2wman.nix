54ea9266f0e2542eaaf187c6c2308e0a2d48d8dc
SnO2WMaN <me@sno2wman.net>
2022-08-22 02:34:20 +0900

    add version

--- sno2wman.nix
+++ sno2wman.nix
@@ -1,9 +1,15 @@
-{pkgs, ...}:
-pkgs.satyxin.buildPackage {
+{
+  pkgs,
+  satyxin,
+  satyxinPackages,
+  ...
+}:
+satyxin.buildPackage {
   name = "sno2wman";
+  version = "1.0.0";
+  outdir = "sno2wman";
   src = ./.;
   sources = {
     dirs = ["./src"];
   };
-  deps = [];
 }
